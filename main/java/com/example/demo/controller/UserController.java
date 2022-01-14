536c1dcabcb4bb1ac402cb70d3baebc9b8c4cb3a
hiitesh1127 <72217750+hiitesh1127@users.noreply.github.com>
2022-01-15 03:24:21 +0530

    Add files via upload

---
+++ main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user")
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping("/user")
+    public String saveUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable int id) {
+        return userService.deleteUser(id);
+    }
+}
---
+++ main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public String saveUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User Saved";
+    }
+
+    public String deleteUser(int id) {
+        if(userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return "User deleted Successfully";
+        } else {
+            return "User does not Exist";
+        }
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUserName(username);
+    }
+
+}
